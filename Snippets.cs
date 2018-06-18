320f565cd070391855ba60778c3bfcf6c123a57c
Geoff <Geoffrey.V.Herz@gmail.com>
2018-06-17 17:22:39 -0700

    Update Snippets.cs

--- Snippets.cs
+++ Snippets.cs
@@ -1,7 +1,7 @@
 // Neat syntax that I've researched about or come up with. 
 // This is all snippet code. The file isn't meant to be run as a whole.
 
-
+//Study on C#'s foreach methods
 var Arr_Of_Strings = new List<string>(){"Hello!", "World!", "--------", "        "};
 foreach(string i in Arr_Of_Strings) Console.WriteLine(i);
 Arr_Of_Strings.ForEach(i => Console.WriteLine(i));
